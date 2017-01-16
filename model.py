a4839c0b3910b17cf7dde2ba85bb031b7b77e7f9
ryoito <r0735nj5058@gmail.com>
2017-01-16 19:05:30 +0900

    confirmed that training proceeds

--- model.py
+++ model.py
@@ -14,11 +14,12 @@ class VoxResModule(chainer.Chain):
     output
     """
     def __init__(self, in_channels):
+        init = chainer.initializers.HeNormal(scale=0.01)
         super(VoxResModule, self).__init__(
             bnorm1=L.BatchNormalization(size=in_channels),
-            conv1=L.ConvolutionND(3, in_channels, 64, 3, pad=1),
+            conv1=L.ConvolutionND(3, in_channels, 64, 3, pad=1, initialW=init),
             bnorm2=L.BatchNormalization(size=64),
-            conv2=L.ConvolutionND(3, 64, 64, 3, pad=1))
+            conv2=L.ConvolutionND(3, 64, 64, 3, pad=1, initialW=init))
 
     def __call__(self, x, train):
         h = F.relu(self.bnorm1(x, test=not train))
@@ -31,32 +32,34 @@ class VoxResModule(chainer.Chain):
 class VoxResNet(chainer.Chain):
     """Voxel Residual Network"""
     def __init__(self, n_classes=4):
-        self.n_classes = n_classes
+        init = chainer.initializers.HeNormal(scale=0.01)
         super(VoxResNet, self).__init__(
-            conv1a=L.ConvolutionND(3, 1, 32, 3, pad=1),
+            conv1a=L.ConvolutionND(3, 1, 32, 3, pad=1, initialW=init),
             bnorm1a=L.BatchNormalization(32),
-            conv1b=L.ConvolutionND(3, 32, 32, 3, pad=1),
+            conv1b=L.ConvolutionND(3, 32, 32, 3, pad=1, initialW=init),
             bnorm1b=L.BatchNormalization(32),
-            conv1c=L.ConvolutionND(3, 32, 64, 3, stride=2, pad=1),
+            conv1c=L.ConvolutionND(3, 32, 64, 3, stride=2, pad=1, initialW=init),
             voxres2=VoxResModule(64),
             voxres3=VoxResModule(64),
             bnorm3=L.BatchNormalization(64),
-            conv4=L.ConvolutionND(3, 64, 64, 3, stride=2, pad=1),
+            conv4=L.ConvolutionND(3, 64, 64, 3, stride=2, pad=1, initialW=init),
             voxres5=VoxResModule(64),
             voxres6=VoxResModule(64),
             bnorm6=L.BatchNormalization(64),
-            conv7=L.ConvolutionND(3, 64, 64, 3, stride=2, pad=1),
+            conv7=L.ConvolutionND(3, 64, 64, 3, stride=2, pad=1, initialW=init),
             voxres8=VoxResModule(64),
             voxres9=VoxResModule(64),
-            deconv1=L.DeconvolutionND(3, 32, 32, 3, pad=1),
-            conv1=L.DeconvolutionND(3, 32, 4, 3, pad=1),
-            deconv2=L.DeconvolutionND(3, 64, 4, 3),
-            deconv4=L.DeconvolutionND(3, 64, 4, 3),
-            deconv8=L.DeconvolutionND(3, 64, 4, 3)
+            c1deconv=L.DeconvolutionND(3, 32, 32, 3, pad=1, initialW=init),
+            c1conv=L.ConvolutionND(3, 32, n_classes, 3, pad=1, initialW=init),
+            c2deconv=L.DeconvolutionND(3, 64, 64, 4, stride=2, pad=1, initialW=init),
+            c2conv=L.ConvolutionND(3, 64, n_classes, 3, pad=1, initialW=init),
+            c3deconv=L.DeconvolutionND(3, 64, 64, 6, stride=4, pad=1, initialW=init),
+            c3conv=L.ConvolutionND(3, 64, n_classes, 3, pad=1, initialW=init),
+            c4deconv=L.DeconvolutionND(3, 64, 64, 10, stride=8, pad=1, initialW=init),
+            c4conv=L.ConvolutionND(3, 64, n_classes, 3, pad=1, initialW=init)
         )
-        self.train = False
 
-    def __call__(self, x):
+    def __call__(self, x, train=False):
         """
         calculate output of VoxResNet given input x
 
@@ -71,21 +74,31 @@ class VoxResNet(chainer.Chain):
             logit to be passed to softmax activation
         """
         h = self.conv1a(x)
-        h = F.relu(self.bnorm1a(h, test=not self.train))
+        h = F.relu(self.bnorm1a(h, test=not train))
         h = self.conv1b(h)
-        c1 = self.deconv1(h)
-        c1 = self.conv1(c1)
-        # h = F.relu(self.bnorm1b(h1, test=not self.train))
-        # h = self.conv1c(h)
-        # h = self.voxres2(h, self.train)
-        # h2 = self.voxres3(h, self.train)
-        # h = F.relu(self.bnorm3(h2, test=not self.train))
-        # h = self.conv4(h)
-        # h = self.voxres5(h, self.train)
-        # h3 = self.voxres6(h, self.train)
-        # h = F.relu(self.bnorm6(h3, test=not self.train))
-        # h = self.conv7(h)
-        # h = self.voxres8(h, self.train)
-        # h4 = self.voxres9(h, self.train)
-        return c1
-        # return F.reshape(F.transpose(c1, (0, 2, 3, 4, 1)), (-1, self.n_classes))
+        c1 = F.clipped_relu(self.c1deconv(h))
+        c1 = self.c1conv(c1)
+
+        h = F.relu(self.bnorm1b(h, test=not train))
+        h = self.conv1c(h)
+        h = self.voxres2(h, train)
+        h = self.voxres3(h, train)
+        c2 = F.clipped_relu(self.c2deconv(h))
+        c2 = self.c2conv(c2)
+
+        h = F.relu(self.bnorm3(h, test=not train))
+        h = self.conv4(h)
+        h = self.voxres5(h, train)
+        h = self.voxres6(h, train)
+        c3 = F.clipped_relu(self.c3deconv(h))
+        c3 = self.c3conv(c3)
+
+        h = F.relu(self.bnorm6(h, test=not train))
+        h = self.conv7(h)
+        h = self.voxres8(h, train)
+        h = self.voxres9(h, train)
+        c4 = F.clipped_relu(self.c4deconv(h))
+        c4 = self.c4conv(c4)
+
+        c = c1 + c2 + c3 + c4
+        return (c1, c2, c3, c4, c)
--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 import argparse
 import pandas as pd
+import numpy as np
 
 import chainer
-import chainer.links as L
-from chainer import training
-from chainer.training import extensions
+from chainer import cuda, Variable
+import chainer.functions as F
 
 import load
 from model import VoxResNet
@@ -14,47 +14,57 @@ from model import VoxResNet
 def main():
     parser = argparse.ArgumentParser(description="train VoxResNet")
     parser.add_argument(
-        "--epoch", "-e", default=int(1e2),
-        help="number of sweeps over the dataset to train")
+        "--iteration", "-i", default=100000, type=int,
+        help="number of iterations")
     parser.add_argument(
-        "--gpu", "-g", default=-1,
+        "--display_step", "-s", default=1000, type=int, help="number of steps to display")
+    parser.add_argument(
+        "--gpu", "-g", default=-1, type=int,
         help="negative value indicates no gpu")
     parser.add_argument(
         "--input", type=str, default="dataset_train.csv",
         help="csv file containing filename of image and its label")
-    parser.add_argument("--n_batch", type=int, default=1, help="batch size")
+    parser.add_argument("--n_batch", type=int, default=5, help="batch size")
     parser.add_argument(
-        "--input_shape", type=int, nargs='*', action="store",
+        "--shape", type=int, nargs='*', action="store",
         default=[80, 80, 80],
         help="shape of input for the network")
     parser.add_argument(
-        '--out', '-o', default='result',
-        help='Directory to output the result')
+        '--out', '-o', default='vrn.npz', type=str,
+        help='trained model')
 
     args = parser.parse_args()
     print(args)
     train_df = pd.read_csv(args.input)
 
     vrn = VoxResNet(n_classes=4)
-    model = L.Classifier(vrn)
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
-        model.to_gpu()
+        vrn.to_gpu()
+    xp = cuda.cupy if args.gpu >= 0 else np
 
-    optimizer = chainer.optimizers.Adam()
-    optimizer.setup(model)
+    optimizer = chainer.optimizers.Adam(eps=1e-5)
+    optimizer.use_cleargrads()
+    optimizer.setup(vrn)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(0.0005))
 
-    train = load.DatasetFromFiles(train_df, 4, args.input_shape)
+    for i in range(args.epoch):
+        vrn.cleargrads()
+        scalar_img, label_img = load.sample(train_df, args.n_batch, args.shape)
+        x_train = Variable(xp.asarray(scalar_img))
+        y_train = Variable(xp.asarray(label_img))
+        outputs = vrn(x_train, train=True)
+        loss = 0
+        for out in outputs:
+            loss += F.softmax_cross_entropy(out, y_train)
+        accuracy_c1 = F.accuracy(outputs[0], y_train)
+        accuracy = F.accuracy(outputs[-1], y_train)
+        loss.backward()
+        optimizer.update()
+        print("step %5d, accuracy_c1 %.02f, accuracy %.02f, cost %f" % (i, accuracy_c1.data, accuracy.data, loss.data))
 
-    train_iter = chainer.iterators.SerialIterator(train, args.n_batch)
-
-    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
-
-    trainer.extend(extensions.dump_graph('main/loss'))
-    trainer.extend(extensions.LogReport())
-    trainer.extend(extensions.PrintReport(['epoch', 'main/loss']))
-    trainer.run()
+    vrn.to_cpu()
+    chainer.serializers.save_npz(args.out, vrn)
 
 
 if __name__ == '__main__':
