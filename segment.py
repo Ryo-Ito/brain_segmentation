8a9f850289458c17337ab17aae20c297d9627eca
Ryo-Ito <r0735nj5058@gmail.com>
2017-03-27 14:47:38 +0900

    json file input

--- segment.py
+++ segment.py
@@ -1,71 +1,75 @@
 import argparse
 import itertools
-import numpy as np
-import nibabel as nib
+import json
+import os
 
 import chainer
-from chainer import cuda, Variable
+import numpy as np
+import nibabel as nib
+import pandas as pd
 
 from load import load_nifti
 from model import VoxResNet
 
 
 def main():
-    parser = argparse.ArgumentParser(description="perform segmentation using trained VoxResNet")
+    parser = argparse.ArgumentParser(description="segment with VoxResNet")
     parser.add_argument(
-        "--input", "-i", type=str,
-        help="brain image to segment")
+        "--input_file", "-i", type=str,
+        help="input json file of test dataset")
     parser.add_argument(
-        "--in_channels", type=int, default=2,
-        help="number of input image channels, default=2")
-    parser.add_argument(
-        "--n_classes", "-c", type=int, default=4,
-        help="number of classes, default=4")
-    parser.add_argument(
-        "--output", "-o", type=str, default="out.nii.gz",
-        help="result of the segmentation, default=out.nii.gz")
+        "--output_suffix", "-o", type=str, default="_segTRI_predict.nii.gz",
+        help="result of the segmentation, default=_segTRI_predict.nii.gz")
     parser.add_argument(
         "--model", "-m", type=str,
         help="a file containing parameters of trained VoxResNet")
     parser.add_argument(
         "--shape", type=int, nargs="*", action="store",
         default=[80, 80, 80],
-        help="patch shape to be passed to VoxResNet, default=[80, 80, 80]")
+        help="input patch shape of VoxResNet, default=[80, 80, 80]")
     parser.add_argument(
         "--gpu", "-g", default=-1, type=int,
         help="negative value indicates no gpu, default=-1")
     args = parser.parse_args()
     print(args)
 
-    vrn = VoxResNet(in_channels=args.in_channels, n_classes=args.n_classes)
+    with open(args.input_file) as f:
+        dataset = json.load(f)
+    test_df = pd.DataFrame(dataset["data"])
+
+    vrn = VoxResNet(dataset["in_channels"], dataset["n_classes"])
     chainer.serializers.load_npz(args.model, vrn)
 
     if args.gpu >= 0:
-        cuda.get_device(args.gpu).use()
+        chainer.cuda.get_device(args.gpu).use()
         vrn.to_gpu()
-    xp = cuda.cupy if args.gpu >= 0 else np
+        xp = chainer.cuda.cupy
+    else:
+        xp = np
 
-    scalar_img, affine = load_nifti(args.input, with_affine=True)
-    if scalar_img.ndim == 3:
-        scalar_img = np.expand_dims(scalar_img, 0)
-    elif scalar_img.ndim == 4:
-        scalar_img = scalar_img.transpose(3, 0, 1, 2)
-    assert scalar_img.ndim == 4, scalar_img.ndim
-    slices = [[], [], []]
-    for img_len, patch_len, slice_ in zip(scalar_img.shape[1:], args.shape, slices):
-        assert img_len > patch_len
-        stride = int((img_len - patch_len) / int(img_len / patch_len))
-        for i in xrange(int(img_len / patch_len)):
-            slice_.append(slice(i * stride, i * stride + patch_len))
-        slice_.append(slice(img_len - patch_len, img_len))
-    output = np.zeros((args.n_classes,) + scalar_img.shape[1:])
-    for xslice, yslice, zslice in itertools.product(*slices):
-        scalar_patch = scalar_img[slice(None), xslice, yslice, zslice]
-        scalar_patch = np.expand_dims(scalar_patch, 0)
-        x = Variable(xp.asarray(scalar_patch))
-        output[slice(None), xslice, yslice, zslice] += cuda.to_cpu(chainer.functions.softmax(vrn(x)).data[0])
-    y = np.argmax(output, axis=0)
-    nib.save(nib.Nifti1Image(y.astype(np.int32), affine), args.output)
+    for image_path, subject in zip(test_df["image"], test_df["subject"]):
+        image, affine = load_nifti(image_path, with_affine=True)
+        image = image.transpose(3, 0, 1, 2)
+        slices = [[], [], []]
+        for img_len, patch_len, slices_ in zip(image.shape[1:], args.shape, slices):
+            assert img_len > patch_len, (img_len, patch_len)
+            stride = int((img_len - patch_len) / int(img_len / patch_len))
+            for i in range(int(img_len / patch_len)):
+                slices_.append(slice(i * stride, i * stride + patch_len))
+            slices_.append(slice(img_len - patch_len, img_len))
+        output = np.zeros((dataset["n_classes"],) + image.shape[1:])
+        for xslice, yslice, zslice in itertools.product(*slices):
+            patch = image[slice(None), xslice, yslice, zslice]
+            patch = np.expand_dims(patch, 0)
+            x = xp.asarray(patch)
+            output[slice(None), xslice, yslice, zslice] += chainer.cuda.to_cpu(
+                vrn(x).data[0])
+        y = np.argmax(output, axis=0)
+        nib.save(
+            nib.Nifti1Image(np.int32(y), affine),
+            os.path.join(
+                os.path.dirname(image_path),
+                subject + args.output_suffix))
 
 
 if __name__ == '__main__':
