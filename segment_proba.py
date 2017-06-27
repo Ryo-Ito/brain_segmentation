731e79a2f4881f05c491c4437ad542d8460153fa
Ryo-Ito <r0735nj5058@gmail.com>
2017-06-27 13:28:07 +0900

    combined probability maps in one file

--- segment_proba.py
+++ segment_proba.py
@@ -18,8 +18,8 @@ parser.add_argument(
     "--input_file", "-i", type=str,
     help="input json file of test dataset")
 parser.add_argument(
-    "--output_suffix", "-o", type=str, default="_segTRI_proba_{}.nii.gz",
-    help="result of the segmentation, default=_segTRI_proba_{}.nii.gz")
+    "--output_suffix", "-o", type=str, default="_segTRI_proba.nii.gz",
+    help="result of the segmentation, default=_segTRI_proba.nii.gz")
 parser.add_argument(
     "--model", "-m", type=str,
     help="a file containing parameters of trained VoxResNet")
@@ -77,9 +77,10 @@ for image_path, subject in zip(test_df["image"], test_df["subject"]):
 
     output /= np.sum(output, axis=0, keepdims=True)
 
-    for i, y in enumerate(output):
-        nib.save(
-            nib.Nifti1Image(y, affine),
-            os.path.join(
-                os.path.dirname(image_path),
-                subject + args.output_suffix.format(i)))
+    nib.save(
+        nib.Nifti1Image(output.transpose(1, 2, 3, 0).astype(np.float32), affine),
+        os.path.join(
+            os.path.dirname(image_path),
+            subject + args.output_suffix
+        )
+    )
