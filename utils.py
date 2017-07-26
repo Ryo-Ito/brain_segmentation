af6e939decc1634abd9292de54f03591a5139ca9
Ryo-Ito <r0735nj5058@gmail.com>
2017-07-26 18:29:07 +0900

    moved function to compute dice coefficient to utils.py

--- utils.py
+++ utils.py
@@ -115,3 +115,19 @@ def crop_patch(image, center, shape):
     patch = image[slices]
     patch = patch.transpose(3, 0, 1, 2)
     return patch
+
+
+def dice_coefficients(label1, label2, labels=None):
+    if labels is None:
+        labels = np.unique(np.hstack((np.unique(label1), np.unique(label2))))
+    dice_coefs = []
+    for label in labels:
+        match1 = (label1 == label)
+        match2 = (label2 == label)
+        denominator = 0.5 * (np.sum(match1.astype(np.float)) + np.sum(match2.astype(np.float)))
+        numerator = np.sum(np.logical_and(match1, match2).astype(np.float))
+        if denominator == 0:
+            dice_coefs.append(np.nan)
+        else:
+            dice_coefs.append(numerator / denominator)
+    return dice_coefs
